8d1e411727953532fcd25a94e60fdcd09eb6af60
Bertrand Huang <weigan.huang@gmail.com>
2012-08-19 11:14:59 +0000

    Initializate FFmpegTest

---
+++ obj/local/armeabi-v7a/objs/ffmpeg-test-jni/ffmpeg-test-jni.o.d
@@ -0,0 +1,254 @@
+/storage/FFmpegTest/obj/local/armeabi-v7a/objs/ffmpeg-test-jni/ffmpeg-test-jni.o: \
+ /storage/FFmpegTest/jni/ffmpeg-test-jni.c \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/jni.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/android/log.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/android/bitmap.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/time.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/sys/time.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/linux/time.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/asm/siginfo.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/asm-generic/siginfo.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/math.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/limits.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/stdio.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/string.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/malloc.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/alloca.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/strings.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/memory.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/inttypes.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/unistd.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/sys/select.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/signal.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/asm/signal.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/asm-generic/signal.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/sys/sysconf.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/linux/capability.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/pathconf.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/assert.h \
+ /storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/avstring.h \
+ /storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/pixdesc.h \
+ /storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/pixfmt.h \
+ /storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/avconfig.h \
+ /storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/imgutils.h \
+ /storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/avutil.h \
+ /storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/common.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/ctype.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/errno.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/linux/errno.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/asm/errno.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/asm-generic/errno.h \
+ /storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/asm-generic/errno-base.h \
+ /storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/attributes.h \
+ /storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/mem.h \
+ /storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/error.h \
+ /storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/mathematics.h \
+ /storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/rational.h \
+ /storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/intfloat_readwrite.h \
+ /storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/log.h \
+ /storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/pixdesc.h \
+ /storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/samplefmt.h \
+ /storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavformat/avformat.h \
+ /storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavcodec/avcodec.h \
+ /storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/avutil.h \
+ /storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/cpu.h \
+ /storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavcodec/version.h \
+ /storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/audioconvert.h \
+ /storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/dict.h \
+ /storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavformat/avio.h \
+ /storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/common.h \
+ /storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/log.h \
+ /storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavformat/version.h \
+ /storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libswscale/swscale.h \
+ /storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavcodec/opt.h \
+ /storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/opt.h \
+ /storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/dict.h \
+ /storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavcodec/avfft.h
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/jni.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/android/log.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/android/bitmap.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/time.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/sys/time.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/linux/time.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/asm/siginfo.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/math.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/limits.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/stdio.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/string.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/strings.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/memory.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/inttypes.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/unistd.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/sys/select.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/signal.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/asm/signal.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/asm-generic/signal.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/sys/sysconf.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/linux/capability.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/pathconf.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/assert.h:
+
+/storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/avstring.h:
+
+/storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/pixdesc.h:
+
+/storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/pixfmt.h:
+
+/storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/avconfig.h:
+
+/storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/imgutils.h:
+
+/storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/avutil.h:
+
+/storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/common.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/ctype.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/errno.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/linux/errno.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/asm/errno.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/asm-generic/errno.h:
+
+/storage/CodeWorkspace/android-ndk-r5b/platforms/android-8/arch-arm/usr/include/asm-generic/errno-base.h:
+
+/storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/attributes.h:
+
+/storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/mem.h:
+
+/storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/error.h:
+
+/storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/mathematics.h:
+
+/storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/rational.h:
+
+/storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/intfloat_readwrite.h:
+
+/storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/log.h:
+
+/storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/pixdesc.h:
+
+/storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/samplefmt.h:
+
+/storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavformat/avformat.h:
+
+/storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavcodec/avcodec.h:
+
+/storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/avutil.h:
+
+/storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/cpu.h:
+
+/storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavcodec/version.h:
+
+/storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/audioconvert.h:
+
+/storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/dict.h:
+
+/storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavformat/avio.h:
+
+/storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/common.h:
+
+/storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/log.h:
+
+/storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavformat/version.h:
+
+/storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libswscale/swscale.h:
+
+/storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavcodec/opt.h:
+
+/storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/opt.h:
+
+/storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavutil/dict.h:
+
+/storage/FFmpegTest/jni/ffmpeg-0.8/android/armv7-a/include/libavcodec/avfft.h:
